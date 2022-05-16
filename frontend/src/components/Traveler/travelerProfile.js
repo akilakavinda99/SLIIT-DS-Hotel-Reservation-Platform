e756ccd10638364b6ca897b67597358d20caa1ea
Thushal Shaminda <it20216146@my.sliit.lk>
2022-05-16 17:27:23 +0530

    traveler profile updated

--- frontend/src/components/Traveler/travelerProfile.js
+++ frontend/src/components/Traveler/travelerProfile.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
 
+import './TravelerProfile.scss';
+
 export default function ViewTraveler(){
 
     const {id} = useParams();
@@ -10,8 +12,8 @@ export default function ViewTraveler(){
     useEffect(() =>{
         const getTraveler =() =>{
         axios.get(`http://localhost:5000/traveler/get/${id}`).then((res) =>{
-            console.log(res)
-            setTraveler(res.data)
+            console.log(res.data)
+            setTraveler(res.data.traveler)
         }).catch((er) =>{
             alert(er.message);
         })
@@ -21,16 +23,61 @@ export default function ViewTraveler(){
 
     return(
         <div>
-            {
-            traveler.map((profile)=>(
-                <ul>
-                    <li>{profile.firstName}</li>
-                    <li>{profile.lastName}</li>
-                    <li>{profile.email}</li>
-                </ul>
+            <div className="profile">
+            <header
+                className="header-main"
+                style={{
+                background:
+                    ' no-repeat center/cover url("/img/admin/admin_main.jpg")',
+                }}
+            >
+                <div className="header-content">
+                    <h2 className="alt-font">Profile</h2>
+                </div>
+            </header>
 
-            ))
-}
+            <div className="register">
+                <div className="container-profile">
+                
+                    <div className="row">
+                        <div className="column">
+                            <label for="first name" class="form-label"><strong>First Name</strong></label>
+                            <input type="text" defaultValue={traveler.firstName} readOnly/>
+                        </div>
+                    
+                        <div className="column">
+                            <label for="last name" class="form-label"><strong>Last Name</strong></label>
+                            <input type="text" defaultValue={traveler.lastName} readOnly/>
+                        </div>
+                    </div>    
+                
+                <div className="row">
+                    <div className="column">
+                        <label for="email" class="form-label"><strong>Email</strong></label>
+                        <input type="text" defaultValue={traveler.email} readOnly/>
+                    </div>
+                
+                    <div className="column">
+                        <label for="telephone no" class="form-label"><strong>Telephone</strong></label>
+                        <input type="text" defaultValue={traveler.telephone} readOnly/>
+                    </div>
+                </div> 
+
+                <div className="row">
+                    <div className="column">
+                        <label for="address" class="form-label"><strong>Address</strong></label>
+                        <input type="text" defaultValue={traveler.address} readOnly/>
+                    </div>
+                </div> 
+
+                <button>my bookings</button>   
+            </div>
+           </div>
+        </div>
         </div>
     )
 }
+
+
+
+
