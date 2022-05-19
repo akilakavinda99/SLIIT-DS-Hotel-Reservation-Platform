3d343aedea35b3c02278856b3766e6b4c28b3c28
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-05-19 10:47:02 +0530

    bookings rooms route change

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "http://localhost:8280/",
 }); // can set a base url here
 
 export const addNewRoom = (data) => API.post("/rooms/create", data);
 export const deleteRoom = (id) => API.post(`/rooms/delete`, id);
-export const fetchRoom = (url) => API.post(`/rooms/${url}`);
+export const fetchRoom = (url) => API.get(`/rooms/get/${url}`);
 export const fetchRooms = () => API.get("/rooms");
 export const fetchAvailableRooms = (data) =>
   API.post("/bookings/available", data);
 export const fetchAllBookings = () => API.get("/bookings");
-export const fetchExistingBooking = (data) => API.post("/bookings", data);
+export const fetchExistingBooking = (data) => API.post("/bookings/get", data);
 export const createBooking = (data) => API.post("/bookings/create", data);
 export const deleteBooking = (data) => API.post("/bookings/delete", data);
 export const signin = (data) => API.post("/auth", data);
