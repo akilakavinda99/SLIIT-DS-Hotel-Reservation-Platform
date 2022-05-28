0513da64337517adc1e09a7cc350f2042d04398f
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-05-28 12:17:29 +0530

    index change

--- payment service/routes/payment.js
+++ payment service/routes/payment.js
@@ -1,29 +1,37 @@
-const router = require("express").Router()
-let Payment = require("../models/paymentModel")
-
+const router = require("express").Router();
+let Payment = require("../models/paymentModel");
+var valid = require("card-validator");
 
 router.route("/add").post((req, res) => {
-
-    const name = req.body.userName
-    const type = req.body.paymentType
-    const email = req.body.email
-    const cardNumber = req.body.cardNumber
-    
+    const name = req.body.userName;
+    const type = req.body.paymentType;
+    const email = req.body.email;
+    const cardNumber = req.body.cardNumber;
 
     const newPayment = new Payment({
         userName: name,
         email: email,
         cardnumber: cardNumber,
-        paymentType: type
-    })
+        paymentType: type,
+    });
+    console.log(cardNumber);
+    var numberValidation = valid.number(cardNumber);
+    console.log(numberValidation);
 
-    newPayment.save().then(() => {
-        res.json("Payment success")
-    }).catch((error) => {
-         res.json(error)
-        console.log(error)
-    })
+    if (numberValidation.isPotentiallyValid) {
+        newPayment
+            .save()
+            .then(() => {
+                console.log("success");
+                res.json("Payment success");
+            })
+            .catch((error) => {
+                res.json(error);
+                console.log(error);
+            });
+    } else {
+        res.json("Payment Failed Invalid Card Number");
+    }
+});
 
-})
-
-module.exports = router
+module.exports = router;
